1b61cef2682a5da8971589408f7e280952afb5bf
Updates-Script <cutenies123@gmail.com>
2025-08-14 12:08:31 +0800

    Create Dual.lua

---
+++ Dual.lua
@@ -0,0 +1,7 @@
+task.spawn(function()
+    loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
+end)
+
+task.spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Updates-Script/Updated/refs/heads/main/Script.lua"))()
+end)
